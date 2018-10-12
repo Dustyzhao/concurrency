01b603ca18eb19a37b7eebf2e6c9044ed1d596df
Dustyzhao <820328544@qq.com>
2018-10-12 18:09:40 +0800

    add annoations

---
+++ src/main/java/com/mmall/concurrency/annoations/NotRecommend.java
@@ -0,0 +1,17 @@
+package com.mmall.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 自定义注解：用来标记【不推荐】的类或者写法，不是方法。
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecommend {
+
+    //自定义注解时，要给个默认值，方便其扩展
+    String value() default "";
+}
---
+++ src/main/java/com/mmall/concurrency/annoations/ThreadSafe.java
@@ -0,0 +1,17 @@
+package com.mmall.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 自定义注解：用来标记【线程安全】的类或者写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadSafe {
+
+    //自定义注解时，要给个默认值，方便其扩展
+    String value() default "";
+}
